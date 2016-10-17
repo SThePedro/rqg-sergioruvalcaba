819a60cd4a402923862e98d5689f436bf53099fc
SThePedroRG21 <thepedro25gonzalez@gmail.com>
2016-10-17 19:54:14 +0000

    first again

---
+++ scripts/main.js
@@ -0,0 +1,14 @@
+var header = document.getElementById("header");
+
+    header.style="background:#777; border: 1px solid #eaea10;";
+    
+var title = document.createElement("h1");
+title.textContent="Random Tip Generator";
+
+    header.appendChild(title);
+    
+    var description = document.createElement("p");description.innerHTML = " This is a description";
+    
+    var h1 = document.getElementById("h1");
+
+    h1.style="color:#555; border: 1px solid #eaea10;";
